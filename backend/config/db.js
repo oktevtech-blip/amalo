3066a07f03c055389764b0b6d0f2b004193a0ecc
Okemaelvis <okemaelvis@gmail.com>
2026-08-13 16:41:40 +0300

    Connect frontend to deployed backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,37 @@
-import mysql from "mysql2/promise";
-import dotenv from "dotenv";
+// import mysql from "mysql2/promise";
+// import dotenv from "dotenv";
 
-dotenv.config();
+// dotenv.config();
 
-const db = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+// const db = mysql.createPool({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME
+// });
+
+// export default db;
+
+const mysql = require("mysql2/promise");
+const path = require("path");
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+
+  ssl: {
+    ca: require("fs").readFileSync(
+      path.join(__dirname, "../certs/ca.pem")
+    ),
+    rejectUnauthorized: true,
+  },
+
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-export default db;
+module.exports = pool;
